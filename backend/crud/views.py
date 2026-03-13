ed8a2801d4428c226d68b32b518ef233537d09cc
Sangam-dev <parajulis295@gmail.com>
2026-03-13 21:19:06 +0545

    added grocerryitem viewset

--- backend/crud/views.py
+++ backend/crud/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import GroceryItem
+from .serializers import GroceryItemSerializer
+
+
+class GroceryItemViewSet(viewsets.ModelViewSet):
+	queryset = GroceryItem.objects.all()
+	serializer_class = GroceryItemSerializer
