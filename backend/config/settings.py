2a17897c9cbd05b04487a901d9daf87e3ec988a5
Sangam-dev <parajulis295@gmail.com>
2026-03-13 23:40:56 +0545

    cors

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -135,4 +135,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
+    'https://crud-drf-psi.vercel.app'
 ]
